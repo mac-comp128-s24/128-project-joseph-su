eb2aa892fbdcacf41b84548bf491dae3e5b0ab87
stkyaw04 <137711211+stkyaw04@users.noreply.github.com>
2024-04-15 11:12:29 -0500

    test

---
+++ src/RedBlackTree.java
@@ -0,0 +1,15 @@
+
+public class RedBlackTree<T> {
+
+    protected BinaryTreeNode<T> root;
+    protected int modCount;
+
+    public RedBlackTree(){
+        root = null;
+    }
+
+    public RedBlackTree(T element){
+
+    }
+    
+}
